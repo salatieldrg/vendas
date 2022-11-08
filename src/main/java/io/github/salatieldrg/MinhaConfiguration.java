ba7b7fa77fcaf192602ea872ac8078150f871323
Salatiel dos Reis <salatieldrg@gmail.com>
2022-11-08 18:55:36 -0300

    configurações de ambientes

--- src/main/java/io/github/salatieldrg/MinhaConfiguration.java
+++ src/main/java/io/github/salatieldrg/MinhaConfiguration.java
@@ -1,13 +1,21 @@
 package io.github.salatieldrg;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
 
     @Bean(name = "applicationName")
     public String applicationName(){
         return "Sistema de Vendas";
+    }
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> System.out.println("Rodando em ambiente de Desenvolvimento");
     }
 }
